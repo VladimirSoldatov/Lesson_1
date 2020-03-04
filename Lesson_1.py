d529de752e203221f6cce5ebec02db573261dc87
Vladimir.Soldatov <cbb110@yandex.ru>
2020-03-05 01:09:01 +0300

    Json

--- Lesson_1.py
+++ Lesson_1.py
@@ -2,7 +2,7 @@
 import requests
 import json
 print('Hello world')
-url = 'https://cbr-xml-daily.ru/dayly.js'
+url = 'https://www.cbr-xml-daily.ru/daily_json.js'
 response = requests.get(url)
 data = json.loads(response.text)
 print(data)
